8c99f5adf6861c6f87afc72d2d5d622938618566
r1khil <rikhil.sharma@outlook.com>
2022-12-06 18:30:59 -0500

    Update apple_1.py

--- apple_1.py
+++ apple_1.py
@@ -4,18 +4,20 @@ import turtle as trtl
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
 
+pear_image = "pear.gif"
 
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
-wn.addshape(apple_image) # Make the screen aware of the new file
+wn.addshape(pear_image) # Make the screen aware of the new file
+wn.bgpic("background.gif")
 
 apple = trtl.Turtle()
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
-  active_apple.shape(apple_image)
+  active_apple.shape(pear_image)
   wn.update()
 
 
